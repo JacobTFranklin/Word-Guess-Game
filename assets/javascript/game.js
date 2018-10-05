cf4158b5395005406bdb0e5b63eee593938e939c
Jacob Franklin <Frank883@umn.edu>
2018-10-05 13:25:33 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,26 @@
  
 
 var selectableWords =  //Word list
-[
-    "birdie",
-    // "clubtwirl",
-    // "sexaddiction",
-    // "perkins",
-    // "masters",
-    // "majors",
-    // "spine",
-    // "eldrick",
-    // "stinger",
-    // "eagle",
-    // "nike",
-    // "chipin",
-    // "jupiter",
-    // "bridgestone",
-    // //"goat",
-]
+    [
+        "birdie",
+        "clubtwirl",
+        "sexaddiction",
+        "perkins",
+        "masters",
+        "majors",
+        "spine",
+        "eldrick",
+        "stinger",
+        "eagle",
+        "nike",
+        "chipin",
+        "jupiter",
+        "bridgestone",
+        "goat",
+    ]
 
 
-const maxTries = 10;
+const maxTries = 10; //Used to set remainingGuesses
 
 var guessedLetters = []; //Holds letters that have already been guessed
 
@@ -29,112 +29,128 @@ var currentWord; //Where we store the word currently being guessed
 
 var guessingWord = []; //Stores properly guessed letters
 
-var remainingGuesses = 10; 
+var remainingGuesses = 10; //How many guesses a user gets per match
 
-var gameOver = true;
+var gameOver = true; //Used for resetting page 
 
-var gameStarted = false;
+var matchFinished = false; //Used for resetting word/guesses
 
-var matchFinished = false;
+var wins = 0; //Sets win counter
 
-var wins = 0;
+currentWord = Math.floor(Math.random() * (selectableWords.length)); //Global variable for selecting random word
 
-var currentWordDiv;
 
-currentWord = Math.floor(Math.random() * (selectableWords.length));
 
 //Functions
 
 
-
-
-//Reset game
+//Reset game: this triggers upon intially loading the page and hitting any key. Wipes wins and resets the match.
 function resetGame(){
+
      gameOver = false;
+
      wins = 0;
+
      resetMatch();
 };
 
-// reset match
+
+//Reset match: this is triggered after a word is correctly guessed. Clears the current word, guessed letters, resets guesses, selects a new random word, and updates the display.
 function resetMatch(){
+
     remainingGuesses = maxTries;
+
     guessedLetters = [];
+
     guessingWord = [];
-    currentWord = Math.floor(Math.random() * (selectableWords.length));
-    for (var i = 0; i < selectableWords[currentWord].length; i++) {
+
+    currentWord = Math.floor(Math.random() * (selectableWords.length)); //Selects a random word
+
+    for (var i = 0; i < selectableWords[currentWord].length; i++) {         //Adds an underscore for each letter in the currentWord
         guessingWord.push(" _ ");
     };
+
     writeDisplay();
 }
 
+
 //Updates the display after guessing a letter
 function writeDisplay(){
-    document.getElementById("totalWins").innerText = wins;
-    document.getElementById("currentWord").innerText = "";
+
+    document.getElementById("totalWins").innerText = wins; //Write the wins count to the totalWins div
+
+    document.getElementById("currentWord").innerText = ""; 
+
     for (var i = 0; i < guessingWord.length; i++) {
-        document.getElementById("currentWord").innerText += guessingWord[i];
+        document.getElementById("currentWord").innerText += guessingWord[i]; //Write out whatever has been guessed
     }
-    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+
+    document.getElementById("remainingGuesses").innerText = remainingGuesses; 
+
     document.getElementById("guessedLetters").innerText = guessedLetters;
-    if(remainingGuesses <=0){
-        // when you run out of guesses a match of hangman has finished
+
+    if(remainingGuesses <=0){           //When you run out of guesses, the match is finished. This will trigger a match reset in the keydown function below once a key is hit.
         matchFinished= true;
     }
 };
 
+
 //If gameOver, start new game on keydown. If game not over, use alphabetic key to trigger guessLetter function.
 document.addEventListener('keydown', function() {
-   if(gameOver){
-    // start of game
+
+   if(gameOver){ //Start of the game, any key resets the game
+    
     resetGame();
+
     gameOver = false;
-   } else if (!matchFinished) {
-        if(event.keyCode >= 65 && event.keyCode <= 90) {
+
+   } else if (!matchFinished) { //Match isn't over, so use alphabetic keys to trigger guess letter function
+        
+   if(event.keyCode >= 65 && event.keyCode <= 90) {
             guessLetter(event.key.toLowerCase());
         };
-    } else if (matchFinished) {
+
+    } else if (matchFinished) { //Once a match is finished, any key will reset the match
         resetMatch();
         matchFinished = false;
     }
 });
 
-function guessLetter(letter){
-    if(remainingGuesses > 0);
-    if (!gameStarted) {
-        gameStarted = true;
-    };
 
-    if (guessedLetters.indexOf(letter) === -1) {
+function guessLetter(letter){ //Pass typed letter into function
+
+    if(remainingGuesses > 0);
+
+    if (guessedLetters.indexOf(letter) === -1) { //If the letter isn't in our guessedLetters array, add it and check the guess
         guessedLetters.push(letter);
         checkGuess(letter);
-}
+    }
+
     writeDisplay();
+
     checkWin();
 };
 
 
 function checkGuess(letter){
-    //Store positions of letters in string
-    var positions = [];
+    
+    var positions = []; //Store positions of letters in an array
 
-
-
-    //find all instances of guessed letter and store in above array
-    for (var i = 0; i < selectableWords[currentWord].length; i++) {
+    
+    for (var i = 0; i < selectableWords[currentWord].length; i++) {  //Find all instances of guessed letter and store in above array
         if(selectableWords[currentWord][i] === letter) {
             positions.push(i);
         }
     }
    
 
-    //remove guess if wrong
+    //Remove guess if wrong
     if (positions.length <= 0) {
         remainingGuesses--;
         document.getElementById("remainingGuesses").innertext = remainingGuesses;
 
     } else {
-        // Replace underscores with letter based on positionsarray
-        for(var i = 0; i < positions.length; i++) {
+        for(var i = 0; i < positions.length; i++) { // Replace underscores with letter based on positionsarray
             guessingWord[positions[i]] = letter;
         }
     }
@@ -144,10 +160,9 @@ function checkGuess(letter){
 
 //Record wins
 function checkWin() {
+
     if(guessingWord.indexOf(" _ ") === -1) {
         wins++;
-        //Need to write to wins div here
-        // enf of a hangman match
         matchFinished = true;
     }
 };
